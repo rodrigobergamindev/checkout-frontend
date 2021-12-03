6c4480c528daac947c578e20bb2164b1278638cc
Rodrigo Bergamin <rodrigobergamindev@gmail.com>
2021-12-03 16:56:06 -0300

    .

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,24 @@
 import '../styles/globals.css'
+import Script from 'next/script'
+import { useEffect } from 'react'
+
+
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  useEffect(() => {
+    const {MercadoPago} = window
+
+    console.log(MercadoPago)
+  },[])
+  
+  return (
+    <>
+  <Script src="https://sdk.mercadopago.com/js/v2"/>
+  <Component {...pageProps} />
+  </>
+  )
 }
 
 export default MyApp
