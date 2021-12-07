9ddfcc46d48c78a9f5b9e004f37d085996799c23
Rodrigo Bergamin <rodrigobergamindev@gmail.com>
2021-12-07 17:07:09 -0300

    .

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,31 +2,36 @@ import Head from 'next/head'
 import styles from '../styles/home.module.css'
 import { useState, useEffect } from 'react'
 import axios from 'axios'
-import { loadStripe } from "@stripe/stripe-js";
-import { Elements } from "@stripe/react-stripe-js";
 import {useRouter} from 'next/router'
 
-const stripePromise = loadStripe("pk_test_51JfA7GHvPtWhJQ2zkDI5cQoMdf1fc3BSkScga962lhQweGAgywYehCEsbGj9glrfIxRC3Edp58YInXDFVW6Tzj7W00vtPCzY48");
+
+
 
 
 
 export default function Home() {
+
+  const [initPoint, setInitPoint] = useState('')
+
 
   const router = useRouter()
 
   async function handleSubmit(event) {
     event.preventDefault()
 
-    const response = await axios.post('http://localhost:3333/create_payment', {
+    const response = await axios.post('http://localhost:3333/payment_mercadopago', {
         title: 'Pulseira banhada',
         unit_price: 89,
         quantity: 2
+    },
+    {
+      headers: { "Content-Type": "application/json" },
     })
     
-    if(response.data){
-      router.push(response.data)
-    }
+    setInitPoint(response.data)
+    
   }
+
 
   return (
     <div className={styles.container}>
@@ -36,7 +41,9 @@ export default function Home() {
       </Head>
      <main className={styles.main}> 
         <div className={styles.checkoutBox} id="checkoutBox">
+        
           <button type="button" onClick={handleSubmit}>Pagar</button>
+          {!!initPoint && <a href={initPoint} target="_blank">Pagar com MercadoPago</a>}
         </div>
      </main>
     </div>
